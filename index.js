11c6538bc6f2c2da4af54fef00c99a1ee450da82
Walker Spence <walker.spence@gmail.com>
2022-02-22 23:27:58 -0800

    refactor: format index with prettier

--- index.js
+++ index.js
@@ -8,9 +8,6 @@ const delay = 3
 const frameSpeed = 7
 
 function startDrawing() {
-
-
-
   const zigZagPath = geZigZagPath()
   const rectangulaPath = getRectangularPath(zigZagPath[zigZagPath.length - 1])
   const finalPath = [...zigZagPath, ...rectangulaPath]
@@ -24,32 +21,50 @@ function startDrawing() {
     }, index * frameSpeed)
 
     //erase brush with a delay
-    if (index + delay >= 0) setTimeout(() => {
-      drawPoints(BurshPoints, ' ')
-    }, ((index + delay) * frameSpeed))
+    if (index + delay >= 0)
+      setTimeout(() => {
+        drawPoints(BurshPoints, ' ')
+      }, (index + delay) * frameSpeed)
   })
 
   //clear the screen and remove all log
   setTimeout(() => {
     process.stdout.cursorTo(0, 0)
-    process.stdout.write('\x1Bc');
-  }, ((finalPath.length + delay) * frameSpeed))
+    process.stdout.write('\x1Bc')
+  }, (finalPath.length + delay) * frameSpeed)
 }
 
 function geZigZagPath() {
   //get half circle path
-  const circlePointsLeft = getCirclefPoints(Math.floor(burshWidth / 2), Math.PI / 2, Math.PI * 3 / 2).reverse()
-  const circlePointsRight = getCirclefPoints(Math.floor(burshWidth / 2), Math.PI * 3 / 2, Math.PI * 5 / 2)
+  const circlePointsLeft = getCirclefPoints(
+    Math.floor(burshWidth / 2),
+    Math.PI / 2,
+    (Math.PI * 3) / 2,
+  ).reverse()
+  const circlePointsRight = getCirclefPoints(
+    Math.floor(burshWidth / 2),
+    (Math.PI * 3) / 2,
+    (Math.PI * 5) / 2,
+  )
 
   //points by which the squeegee
   const keyPoints = getKeyPoints()
 
   let points = []
   for (let step = 0; step < keyPoints.length; step++) {
-    const linePoints = getLinePoints(keyPoints[step][0].x, keyPoints[step][0].y, keyPoints[step][1].x, keyPoints[step][1].y)
-    const turnPoints = (step % 2 == 0 ? circlePointsRight : circlePointsLeft).map(point => (
-      { x: point.x + keyPoints[step][1].x, y: point.y + keyPoints[step][1].y + burshWidth / 2, angle: point.angle }
-    ))
+    const linePoints = getLinePoints(
+      keyPoints[step][0].x,
+      keyPoints[step][0].y,
+      keyPoints[step][1].x,
+      keyPoints[step][1].y,
+    )
+    const turnPoints = (
+      step % 2 == 0 ? circlePointsRight : circlePointsLeft
+    ).map((point) => ({
+      x: point.x + keyPoints[step][1].x,
+      y: point.y + keyPoints[step][1].y + burshWidth / 2,
+      angle: point.angle,
+    }))
     points = [...points, ...linePoints, ...turnPoints]
   }
   return points
@@ -57,8 +72,8 @@ function geZigZagPath() {
 
 function getRectangularPath(closestStartPoint) {
   let points = []
-  const verticalMargin = (burshWidth / 2)
-  const horizontalMargin = ((burshWidth * deformationFactor) / 2)
+  const verticalMargin = burshWidth / 2
+  const horizontalMargin = (burshWidth * deformationFactor) / 2
   const startPoint = { x: closestStartPoint.x, y: rows - verticalMargin }
 
   for (let x = startPoint.x; x > -2; x--) {
@@ -66,7 +81,13 @@ function getRectangularPath(closestStartPoint) {
   }
 
   let anglePoints = getCirclefPoints(Math.floor(burshWidth / 2), 0, Math.PI / 2)
-  anglePoints = anglePoints.map(point => ({ x: point.x, y: point.y + rows - verticalMargin * 2, angle: point.angle })).reverse()
+  anglePoints = anglePoints
+    .map((point) => ({
+      x: point.x,
+      y: point.y + rows - verticalMargin * 2,
+      angle: point.angle,
+    }))
+    .reverse()
   points = [...points, ...anglePoints]
 
   for (let y = rows - verticalMargin - 3; y > -1; y--) {
@@ -74,16 +95,34 @@ function getRectangularPath(closestStartPoint) {
     points.push({ y, x: horizontalMargin - 1, angle: Math.PI })
   }
 
-  let anglePoints2 = getCirclefPoints(Math.floor(burshWidth / 2), Math.PI, Math.PI * 3 / 2).reverse()
-  anglePoints2 = anglePoints2.map(point => ({ x: point.x + horizontalMargin * 2, y: point.y, angle: point.angle })).reverse()
+  let anglePoints2 = getCirclefPoints(
+    Math.floor(burshWidth / 2),
+    Math.PI,
+    (Math.PI * 3) / 2,
+  ).reverse()
+  anglePoints2 = anglePoints2
+    .map((point) => ({
+      x: point.x + horizontalMargin * 2,
+      y: point.y,
+      angle: point.angle,
+    }))
+    .reverse()
   points = [...points, ...anglePoints2]
 
   for (let x = horizontalMargin + 3; x < columns; x++) {
     points.push({ y: verticalMargin - 1, x, angle: Math.PI / 2 })
   }
 
-  let anglePoints3 = getCirclefPoints(Math.floor(burshWidth / 2), Math.PI, Math.PI * 3 / 2).reverse()
-  anglePoints3 = anglePoints3.map(point => ({ x: point.x + columns, y: point.y + verticalMargin * 2, angle: point.angle }))
+  let anglePoints3 = getCirclefPoints(
+    Math.floor(burshWidth / 2),
+    Math.PI,
+    (Math.PI * 3) / 2,
+  ).reverse()
+  anglePoints3 = anglePoints3.map((point) => ({
+    x: point.x + columns,
+    y: point.y + verticalMargin * 2,
+    angle: point.angle,
+  }))
   points = [...points, ...anglePoints3]
 
   for (let y = verticalMargin + 3; y < rows; y++) {
@@ -91,13 +130,22 @@ function getRectangularPath(closestStartPoint) {
     points.push({ y, x: columns - horizontalMargin, angle: Math.PI })
   }
 
-  let anglePoints4 = getCirclefPoints(Math.floor(burshWidth / 2), Math.PI * 3 / 2, Math.PI * 4 / 2)
-  anglePoints4 = anglePoints4.map(point => ({ x: point.x + columns - horizontalMargin * 2, y: point.y + rows, angle: point.angle })).reverse()
+  let anglePoints4 = getCirclefPoints(
+    Math.floor(burshWidth / 2),
+    (Math.PI * 3) / 2,
+    (Math.PI * 4) / 2,
+  )
+  anglePoints4 = anglePoints4
+    .map((point) => ({
+      x: point.x + columns - horizontalMargin * 2,
+      y: point.y + rows,
+      angle: point.angle,
+    }))
+    .reverse()
   points = [...points, ...anglePoints4]
 
   for (let x = columns - horizontalMargin - 3; x > startPoint.x; x--) {
     points.push({ y: rows - verticalMargin, x, angle: Math.PI / 2 })
-
   }
   return points
 }
@@ -105,8 +153,16 @@ function getRectangularPath(closestStartPoint) {
 function getCirclefPoints(radius, start, end) {
   const angleStep = 5
   let points = []
-  for (let angle = start; angle < end; angle += (2 * Math.PI / 360) * angleStep) {
-    points.push({ x: Math.cos(angle) * radius * deformationFactor, y: Math.sin(angle) * radius, angle })
+  for (
+    let angle = start;
+    angle < end;
+    angle += ((2 * Math.PI) / 360) * angleStep
+  ) {
+    points.push({
+      x: Math.cos(angle) * radius * deformationFactor,
+      y: Math.sin(angle) * radius,
+      angle,
+    })
   }
   return points
 }
@@ -116,9 +172,21 @@ function getKeyPoints() {
   const halfBrushDeformed = (burshWidth * deformationFactor) / 2
   let step = 0
 
-  while (((burshWidth / 2) * 3 + (step - 1) * burshWidth) < rows) {
-    points.push([{ x: halfBrushDeformed * 2, y: (burshWidth / 2) * 2 + step * burshWidth }, { x: columns - halfBrushDeformed * 2, y: (burshWidth / 2) * 1 + step * burshWidth }])
-    points.push([{ x: columns - halfBrushDeformed * 2, y: (burshWidth / 2) * 3 + step * burshWidth }, { x: halfBrushDeformed * 2, y: (burshWidth / 2) * 2 + step * burshWidth }])
+  while ((burshWidth / 2) * 3 + (step - 1) * burshWidth < rows) {
+    points.push([
+      { x: halfBrushDeformed * 2, y: (burshWidth / 2) * 2 + step * burshWidth },
+      {
+        x: columns - halfBrushDeformed * 2,
+        y: (burshWidth / 2) * 1 + step * burshWidth,
+      },
+    ])
+    points.push([
+      {
+        x: columns - halfBrushDeformed * 2,
+        y: (burshWidth / 2) * 3 + step * burshWidth,
+      },
+      { x: halfBrushDeformed * 2, y: (burshWidth / 2) * 2 + step * burshWidth },
+    ])
     step++
   }
   return points
@@ -130,26 +198,44 @@ function getLinePoints(startX, startY, endX, endY) {
   const Xdirection = endX > startX ? 1 : -1
   const angle = -Math.atan((endY - startX) / startY - endY)
   for (let step = 0; step < Math.abs(endX - startX); step++) {
-    points.push({ x: step * Xdirection + startX, y: startY + Ystep * step, angle })
+    points.push({
+      x: step * Xdirection + startX,
+      y: startY + Ystep * step,
+      angle,
+    })
   }
   return points
 }
 
 function getBrushPoints(x, y, angle) {
-  let newX = 0;
-  let newY = 0;
+  let newX = 0
+  let newY = 0
   let points = []
   const halfBurshWidth = burshWidth / 2
 
   const oppositeAngle = angle + Math.PI
 
   for (let step = 0; step < halfBurshWidth * deformationFactor; step++) {
-    newX = x + (Math.cos(angle) * (halfBurshWidth / (halfBurshWidth * deformationFactor) * step) * deformationFactor)
-    newY = y + (Math.sin(angle) * (halfBurshWidth / (halfBurshWidth * deformationFactor) * step))
+    newX =
+      x +
+      Math.cos(angle) *
+        ((halfBurshWidth / (halfBurshWidth * deformationFactor)) * step) *
+        deformationFactor
+    newY =
+      y +
+      Math.sin(angle) *
+        ((halfBurshWidth / (halfBurshWidth * deformationFactor)) * step)
     points.push({ x: newX, y: newY })
 
-    newX = x + (Math.cos(oppositeAngle) * (halfBurshWidth / (halfBurshWidth * deformationFactor) * step) * deformationFactor)
-    newY = y + (Math.sin(oppositeAngle) * (halfBurshWidth / (halfBurshWidth * deformationFactor) * step))
+    newX =
+      x +
+      Math.cos(oppositeAngle) *
+        ((halfBurshWidth / (halfBurshWidth * deformationFactor)) * step) *
+        deformationFactor
+    newY =
+      y +
+      Math.sin(oppositeAngle) *
+        ((halfBurshWidth / (halfBurshWidth * deformationFactor)) * step)
     points.push({ x: newX, y: newY })
   }
   return points
@@ -161,10 +247,10 @@ function drawnStringAt(x, y, str) {
 }
 
 function drawPoints(list, character = '#') {
-  list.forEach(point => {
-    if (point.y < rows && point.x < columns) drawnStringAt(point.x, point.y, character)
+  list.forEach((point) => {
+    if (point.y < rows && point.x < columns)
+      drawnStringAt(point.x, point.y, character)
   })
 }
 
 startDrawing()
-
