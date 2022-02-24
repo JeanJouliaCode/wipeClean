0148271b825d70cb48b269e3c81c119c33945ea8
Walker Spence <walker.spence@gmail.com>
2022-02-23 18:38:26 -0800

    fix: handle writeFile error

--- index.js
+++ index.js
@@ -289,8 +289,12 @@ function safeReadConfig(filePath) {
 function updateSavedSpeed(newSpeed) {
   safeCreateDirectory(CONFIG_DIRECTORY)
   safeCreateDirectory(WIPECLEAN_CONFIG_DIRECTORY)
-  writeFileSync(CONFIG_FILE, JSON.stringify({ speed: newSpeed }))
-  console.log(`Updated brush speed to ${newSpeed} fps`)
+  try {
+    writeFileSync(CONFIG_FILE, JSON.stringify({ speed: newSpeed }))
+    console.log(`Updated brush speed to ${newSpeed} fps.`)
+  } catch (e) {
+    console.log('Failed to update brush speed:', e)
+  }
 }
 
 function drawWithSavedSpeed() {
