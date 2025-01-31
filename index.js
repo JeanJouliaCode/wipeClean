5bfceb4d501b2f7536bb0e65902b7da7439b0e97
Roffi <ra99kuhn+GitHub@gmail.com>
2025-01-31 17:19:15 +0100

    Squashed commit of the following:
    commit 29ed53782784fd19ba94e2cbc0d047bf08f841e6
    Author: Roffi <ra99kuhn+GitHub@gmail.com>
    Date:   Fri Jan 31 17:07:28 2025 +0100
    
        Fixed typos, thanks to aaronjamt
        Pulled from github.com/aaronjamt/wipeClean
    
        Thanks also to Jjman739 who fixed the same typos in the past already.
    
        Squashed commit of the following:
    
        commit 97971d3ad8d37f370faff37e4469cc1e65b3a7e1
        Author: Aaronjamt <13600347+aaronjamt@users.noreply.github.com>
        Date:   Tue Mar 22 08:58:13 2022 -0700
    
            'drawnStringAt' -> 'drawStringAt'
    
        commit 6385598cd1a99f91c95d730b49b845797e76b280
        Author: Aaronjamt <13600347+aaronjamt@users.noreply.github.com>
        Date:   Mon Mar 21 17:51:31 2022 -0700
    
            Fix one more typo (missed rectangulaPath)
    
        commit 7c5aacb0f392590569d614c3ab9fe874a1ac6446
        Author: Aaronjamt <13600347+aaronjamt@users.noreply.github.com>
        Date:   Mon Mar 21 17:47:16 2022 -0700
    
            Fix minor typos (bursh and circlef)
    
    commit ab612ac70c42df6e16d311b3e0299e78907dc96e
    Author: Roffi <ra99kuhn+GitHub@gmail.com>
    Date:   Fri Jan 31 15:47:28 2025 +0100
    
        Fixed typos, thanks to creeper6530
        Pulled from github.com/creeper6530/wipeClean
    
        Squashed commit of the following:
    
        commit f0649faf082c459e9d9f0020a0cd7e304dee9533
        Author: creeper6530 <77244144+creeper6530@users.noreply.github.com>
        Date:   Mon Aug 28 18:25:57 2023 +0200
    
            Another typo, I guess
    
        commit 891afd9105d0c9bbdcda797d24ec5e0313dad1ee
        Author: creeper6530 <77244144+creeper6530@users.noreply.github.com>
        Date:   Mon Aug 28 18:23:15 2023 +0200
    
            Fixed a typo

--- index.js
+++ index.js
@@ -17,21 +17,21 @@ const ROWS = process.stdout.rows
 function startDrawing(speed = 150) {
   const msPerFrame = 1000 / speed
   const zigZagPath = getZigZagPath()
-  const rectangulaPath = getRectangularPath(zigZagPath[zigZagPath.length - 1])
-  const finalPath = [...zigZagPath, ...rectangulaPath]
+  const rectangularPath = getRectangularPath(zigZagPath[zigZagPath.length - 1])
+  const finalPath = [...zigZagPath, ...rectangularPath]
 
   finalPath.forEach((point, index) => {
-    const BurshPoints = getBrushPoints(point.x, point.y, point.angle)
+    const BrushPoints = getBrushPoints(point.x, point.y, point.angle)
 
     //draw Brush
     setTimeout(() => {
-      drawPoints(BurshPoints)
+      drawPoints(BrushPoints)
     }, index * msPerFrame)
 
     //erase brush with a DELAY
     if (index + DELAY >= 0)
       setTimeout(() => {
-        drawPoints(BurshPoints, ' ')
+        drawPoints(BrushPoints, ' ')
       }, (index + DELAY) * msPerFrame)
   })
 
@@ -44,12 +44,12 @@ function startDrawing(speed = 150) {
 
 function getZigZagPath() {
   //get half circle path
-  const circlePointsLeft = getCirclefPoints(
+  const circlePointsLeft = getCirclePoints(
     Math.floor(BRUSH_WIDTH / 2),
     Math.PI / 2,
     (Math.PI * 3) / 2,
   ).reverse()
-  const circlePointsRight = getCirclefPoints(
+  const circlePointsRight = getCirclePoints(
     Math.floor(BRUSH_WIDTH / 2),
     (Math.PI * 3) / 2,
     (Math.PI * 5) / 2,
@@ -88,7 +88,7 @@ function getRectangularPath(closestStartPoint) {
     points.push({ y: ROWS - verticalMargin + 1, x, angle: Math.PI / 2 })
   }
 
-  let anglePoints = getCirclefPoints(
+  let anglePoints = getCirclePoints(
     Math.floor(BRUSH_WIDTH / 2),
     0,
     Math.PI / 2,
@@ -107,7 +107,7 @@ function getRectangularPath(closestStartPoint) {
     points.push({ y, x: horizontalMargin - 1, angle: Math.PI })
   }
 
-  let anglePoints2 = getCirclefPoints(
+  let anglePoints2 = getCirclePoints(
     Math.floor(BRUSH_WIDTH / 2),
     Math.PI,
     (Math.PI * 3) / 2,
@@ -125,7 +125,7 @@ function getRectangularPath(closestStartPoint) {
     points.push({ y: verticalMargin - 1, x, angle: Math.PI / 2 })
   }
 
-  let anglePoints3 = getCirclefPoints(
+  let anglePoints3 = getCirclePoints(
     Math.floor(BRUSH_WIDTH / 2),
     Math.PI,
     (Math.PI * 3) / 2,
@@ -142,7 +142,7 @@ function getRectangularPath(closestStartPoint) {
     points.push({ y, x: COLUMNS - horizontalMargin, angle: Math.PI })
   }
 
-  let anglePoints4 = getCirclefPoints(
+  let anglePoints4 = getCirclePoints(
     Math.floor(BRUSH_WIDTH / 2),
     (Math.PI * 3) / 2,
     (Math.PI * 4) / 2,
@@ -162,7 +162,7 @@ function getRectangularPath(closestStartPoint) {
   return points
 }
 
-function getCirclefPoints(radius, start, end) {
+function getCirclePoints(radius, start, end) {
   const angleStep = 5
   let points = []
   for (
@@ -259,7 +259,7 @@ function getBrushPoints(x, y, angle) {
   return points
 }
 
-function drawnStringAt(x, y, str) {
+function drawStringAt(x, y, str) {
   process.stdout.cursorTo(Math.round(x), Math.round(y))
   process.stdout.write(str)
 }
@@ -267,7 +267,7 @@ function drawnStringAt(x, y, str) {
 function drawPoints(list, character = '#') {
   list.forEach((point) => {
     if (point.y < ROWS && point.x < COLUMNS)
-      drawnStringAt(point.x, point.y, character)
+      drawStringAt(point.x, point.y, character)
   })
 }
 
